695146047abc39b150c26ac3722a243ac1d57b06
g24k12 <iLabCoding@LenovoLab2.strathmore.local>
2022-04-01 05:40:42 -0700

    working on views

--- games/polls/urls.py
+++ games/polls/urls.py
@@ -2,6 +2,16 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns=[
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+
+    #adding the polls path eg:/pools/2/
+    path("<int:question_id>/",views.detail,name = 'detail'),
+
+    #path for results/polls/2/results
+    path("<int:question_id>/results/",views.results,name = 'results'),
+
+    # path for results/polls/2/vote
+    path("<int:question_id>/vote/",views.vote,name = 'vote')
 ]
--- games/polls/views.py
+++ games/polls/views.py
@@ -1,7 +1,44 @@
-from django.shortcuts import render
+from multiprocessing import context
+from pickle import HIGHEST_PROTOCOL
+from unittest import loader
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
+from django.template import loader
+
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, Welcome to my polls website.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # 1. using a short cut render
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
+
+#2. using a common way to laod a template
+# def index(request):
+#     return HttpResponse("Hello, Welcome to my polls website.")
+
+# def detail(request, question_id):
+#     return HttpResponse("you are loooking at question %s." % question_id)
+
+# def results(request,question_id):
+#     response = "you're loooking at the resuts of question %s."
+#     return HttpResponse(response % question_id)
+
+# def vote(request, question_id):
+#     return HttpResponse("you're voting on question %s."% question_id)
+ 
+def detail(request, question_id):
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, 'polls/detail.html',{'question': question})
+
+#A shortcut: get_object-or_404()
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
